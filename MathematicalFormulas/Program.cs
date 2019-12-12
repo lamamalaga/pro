5834c247215d0cbffd2c90039314edf32f479770
lamamalaga <tomchuk.01@gmail.com>
2019-12-12 14:40:46 +0500

    Математиеские формулы

---
+++ MathematicalFormulas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalFormulas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение переменной X: ");
+            var x = double.Parse(Console.ReadLine());
+
+            var s = Math.Sqrt(x * x + Math.Sqrt(x * x + 1 / x * x));
+
+            Console.WriteLine("Выражение равно "+s);
+
+            Console.ReadLine();
+        }
+    }
+}
