a9b3ed8d4f44c73d85811c493a928d149d55698d
lamamalaga <tomchuk.01@gmail.com>
2019-12-15 21:36:24 +0500

    Логические операции и выражения. Задание 2.

---
+++ Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число: ");
+            var g = Console.ReadLine();
+
+
+
+        }
+    }
+}
