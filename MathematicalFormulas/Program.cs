c4eb6824c717fc8c6ee9cbfd6d5137fbb8a5f6e8
lamamalaga <tomchuk.01@gmail.com>
2019-12-17 01:13:16 +0500

    Исправила задачу Математические формулы.

--- MathematicalFormulas/Program.cs
+++ MathematicalFormulas/Program.cs
@@ -13,7 +13,8 @@ namespace MathematicalFormulas
             Console.WriteLine("Введите значение переменной X: ");
             var x = double.Parse(Console.ReadLine());
 
-            var s = Math.Sqrt(x * x + Math.Sqrt(x * x + 1 / x * x));
+            var d = x * x;
+            var s = Math.Sqrt(d +( Math.Sqrt(d + (1 / d))));
 
             Console.WriteLine("Выражение равно "+s);
 
