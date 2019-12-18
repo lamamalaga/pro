59a429ac74c1dbf352ba4e1bbeaab8ea3fcf02e6
lamamalaga <tomchuk.01@gmail.com>
2019-12-18 17:20:41 +0500

    Логические операции и выражения.Задание 2 Исправленное.

---
+++ LogicalTask2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение X: ");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y: ");
+            var y = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Принадлежность точки графику " + Graphic(x, y));
+            Console.ReadKey();
+
+        }
+
+        static bool Graphic(double x, double y)
+        {
+            return y >-2 && x > 3 && y > -2 && x > -1;
+        }
+       
+    }
+}
--- LogicalTask2/Properties/AssemblyInfo.cs
+++ LogicalTask2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LogicTask2")]
+[assembly: AssemblyTitle("LogicalTask2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LogicTask2")]
+[assembly: AssemblyProduct("LogicalTask2")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ad9c3696-9eb8-4e00-badf-89072f8a99c1")]
+[assembly: Guid("167ca40d-db82-4bcd-96e6-da21646ba14e")]
 
 // Version information for an assembly consists of the following four values:
 //
